b5cffae073e7d1d3ea4623de4c63260ac3db8fbc
Surbhi Singh <surbhisinghtyu36@gmail.com>
2024-09-23 13:25:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(os.environ.get("API_ID", 26930530))
 API_HASH = os.environ.get("API_HASH", "b578cec1f4f5164d952c5a785a399a73")
 
 # Bot token
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7867320428:AAEcEbd8ajs1S2vM5bbyoDMl0YO0frfXO58")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7817835911:AAE8gCzjsKOF4NGcneAGau5DtWtTzbs9TRA")
 
 # Pyrogram session name
 SESSION_NAME = os.environ.get("SESSION_NAME")
